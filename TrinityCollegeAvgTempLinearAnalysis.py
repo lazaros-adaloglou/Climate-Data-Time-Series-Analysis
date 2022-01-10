ff447d6b58ab5a7b5a984c817f5fb3b6da9f15f1
Lazaros Adaloglou <adalogloulazaros@gmail.com>
2022-01-10 20:49:17 +0200

    Read Time Series

--- TrinityCollegeAvgTempLinearAnalysis.py
+++ TrinityCollegeAvgTempLinearAnalysis.py
@@ -1,11 +1,14 @@
 import pandas as pd
-import os
+import numpy as np
 
 df = pd.read_csv("data/TrinityCollege.csv", delimiter=',')
-df = df.loc[7305:, ['date', 'maxt', 'mint']]
-#print(df)
-#print((df.loc[:, "maxt"] == " ").sum())
-#print((df.loc[:, "mint"] == " ").sum())
-print(df.duplicated('date', keep='first'))
+df = df.loc[7305:12734, ['date', 'maxt', 'mint']]
+# print(df)
+# print((df.loc[:, "maxt"] == " ").sum())
+# print((df.loc[:, "mint"] == " ").sum())
 
+for space in np.arange(7305, 12734+1):
+    if df.loc[space, "maxt"] == " " and df.loc[space, "mint"] == " ":
+        df.drop([space], axis=0)
 
+print(df)
