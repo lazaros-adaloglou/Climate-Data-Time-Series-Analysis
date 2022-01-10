43023eb5605dbe9acba4b01aea3bef900bfd4917
Lazaros Adaloglou <adalogloulazaros@gmail.com>
2022-01-10 19:49:31 +0200

    Read Time Series

--- TrinityCollegeAvgTempLinearAnalysis.py
+++ TrinityCollegeAvgTempLinearAnalysis.py
@@ -2,7 +2,9 @@ import pandas as pd
 import os
 
 df = pd.read_csv("data/TrinityCollege.csv", delimiter=',')
-df = df.loc[:, ["maxt", "mint"]]
+df = df.loc[7305:, ["maxt", "mint"]]
+print(df)
 print((df.loc[:, "maxt"] == " ").sum())
+print((df.loc[:, "mint"] == " ").sum())
 
 
