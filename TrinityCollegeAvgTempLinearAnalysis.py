9953eb475bab6bb9b48f00c8e2ede3fa3922c998
korohub97 <97450475+korohub97@users.noreply.github.com>
2022-01-10 20:19:28 +0200

    v1

--- TrinityCollegeAvgTempLinearAnalysis.py
+++ TrinityCollegeAvgTempLinearAnalysis.py
@@ -2,9 +2,10 @@ import pandas as pd
 import os
 
 df = pd.read_csv("data/TrinityCollege.csv", delimiter=',')
-df = df.loc[7305:, ["maxt", "mint"]]
-print(df)
-print((df.loc[:, "maxt"] == " ").sum())
-print((df.loc[:, "mint"] == " ").sum())
+df = df.loc[7305:, ['date', 'maxt', 'mint']]
+#print(df)
+#print((df.loc[:, "maxt"] == " ").sum())
+#print((df.loc[:, "mint"] == " ").sum())
+print(df.duplicated('date', keep='first'))
 
 
