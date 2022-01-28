d51d0019851276ad2ca4257d0913c419b9ba47e3
Lazaros Adaloglou <adalogloulazaros@gmail.com>
2022-01-28 22:02:36 +0200

    Read Time Series

--- DublinAirportAvgTempLinearAnalysis.py
+++ DublinAirportAvgTempLinearAnalysis.py
@@ -1,6 +1,10 @@
 # Imports.
-import os
+import LinearAnalysisFunctions as lf
+from statsmodels.graphics.tsaplots import plot_acf
+import matplotlib.pyplot as plt
 import pandas as pd
+import numpy as np
+import os
 
 filename = 'data/DublinAirport_Data.csv'
 
@@ -14,3 +18,18 @@ print("-------------------------------------------------------------------------
 print("Time Series:")
 data = data.drop("Unnamed: 0", axis=1)
 print(data)
+
+# Define Average Temperature Time Series.
+x = data.AvgTemp
+savepath = 'data/'
+value = 'AvgTemp (°C)'
+
+# Plot Average Temperature.
+lf.plot_timeseries(x, value, 'Average Temperature', savepath)
+plt.show()
+
+# Average Temperature Histogram.
+lf.plot_histogram(x, value, 'Average Temperature', savepath)
+plt.show()
+
+# Remove Trend
