cb343baeda7c3a6557e7051068af5c4bfbde8702
Lazaros Adaloglou <adalogloulazaros@gmail.com>
2022-02-13 22:17:33 +0200

    Read Time Series

--- DublinAirportAvgTempLinearAnalysis.py
+++ DublinAirportAvgTempLinearAnalysis.py
@@ -1,7 +1,8 @@
 # Imports.
 import LinearAnalysisFunctions as lf
-from statsmodels.graphics.tsaplots import plot_acf
+from statsmodels.graphics.tsaplots import plot_acf, plot_pacf
 import matplotlib.pyplot as plt
+import pmdarima as pm
 import pandas as pd
 import numpy as np
 import os
@@ -15,21 +16,46 @@ except FileNotFoundError:
     data = pd.read_csv(filename, delimiter=',', parse_dates=['date'])
 
 print("---------------------------------------------------------------------------------------------------------------")
-print("Time Series:")
+print("Average Temperature Time Series:\n")
 data = data.drop("Unnamed: 0", axis=1)
 print(data)
 
 # Define Average Temperature Time Series.
-x = data.AvgTemp
+x = data.AvgTemp.values
+x_df = pd.DataFrame({data.AvgTemp.name: x})
 savepath = 'data/'
-value = 'AvgTemp (°C)'
+strings = ['AvgTemp (°C)', 'Average Temperature']
 
 # Plot Average Temperature.
-lf.plot_timeseries(x, value, 'Average Temperature', savepath)
+lf.plot_timeseries(x, strings[0], strings[1], savepath)
 plt.show()
 
 # Average Temperature Histogram.
-lf.plot_histogram(x, value, 'Average Temperature', savepath)
+lf.plot_histogram(x, strings[0], strings[1], savepath)
 plt.show()
 
 # Remove Trend.
+window = 15
+ma = lf.rolling_window(x=x, window=window)
+plt.plot(ma, linestyle='--')
+plt.plot(x, alpha=0.5)
+plt.plot(x-ma, alpha=0.5)
+plt.legend([f'MA', 'Original', 'Detrended'])
+plt.show()
+
+# First Differences.
+fd = np.diff(x)
+
+# Comparison.
+plt.plot(x-ma, alpha=0.5)
+plt.plot(fd, alpha=0.5)
+plt.xlim(1000, 2000)
+plt.legend(['ma', 'diffs'])
+plt.show()
+
+# Correlation.
+plot_acf(x-ma, zero=False)
+plt.show()
+
+
+
--- LinearAnalysisFunctions.py
+++ LinearAnalysisFunctions.py
@@ -5,12 +5,11 @@ import numpy as np
 
 
 # Plot Time Series.
-def plot_timeseries(x, value='', title='', savepath='', zoomy=1):
+def plot_timeseries(x, value='', title='', savepath=''):
 
     plt.plot(x, marker='x', linestyle='--', linewidth=1)
     plt.xlabel('Time (Days)')
     plt.ylabel(value)
-    plt.ylim([-zoomy * abs(min(x)) - abs(min(x)), zoomy * abs(max(x)) + 0.2*abs(max(x))])
     if len(title) > 0:
         plt.title(title, x=0.5, y=1.0)
     if len(savepath) > 0:
@@ -27,4 +26,26 @@ def plot_histogram(x, value, title='', savepath=''):
         plt.title(title, x=0.5, y=1.0)
     if len(savepath) > 0:
         plt.savefig(f'{savepath}/{title} Histogram.png')
+
+
+# Returns the Moving Average of a Time Series x with Length of Window.
+def rolling_window(x, window):
+
+    x = x.flatten()
+    return np.convolve(x, np.ones(window) / window, mode='same')
+
+
+# Fit to a given time series with a polynomial of a given order. x: vector of length 'n' of the time series
+# p: the order of the polynomial to be fitted. return: vector of length 'n' of the fitted time series.
+def polynomial_fit(x, p):
+
+    n = x.shape[0]
+    x = x[:]
+    if p > 1:
+        tv = np.arange(n)
+        bv = np.polyfit(x=tv, y=x, deg=p)
+        muv = np.polyval(p=bv, x=tv)
+    else:
+        muv = np.full(shape=n, fill_value=np.nan)
+    return muv
 
