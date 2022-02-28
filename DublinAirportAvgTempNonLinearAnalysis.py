58adf8cccd4b915d95157d448b5b2500b0f701b1
Lazaros Adaloglou <adalogloulazaros@gmail.com>
2022-02-28 13:40:59 +0200

    Read Time Series

--- DublinAirportAvgTempNonLinearAnalysis.py
+++ DublinAirportAvgTempNonLinearAnalysis.py
@@ -1,15 +1,347 @@
 # Imports.
-import os
+from statsmodels.tsa.arima_process import arma_generate_sample, arma_acf, arma_pacf
+from statsmodels.tsa.stattools import acf, pacf, adfuller
+from statsmodels.graphics.tsaplots import plot_acf
+from nolitsa import delay, dimension, d2, utils
+from sklearn.metrics import mean_squared_error
+import NonLinearAnalysisFunctions as nlf
+import LinearAnalysisFunctions as lf
+from matplotlib import pyplot as plt
+from sklearn.neighbors import KDTree
+from statsmodels.api import OLS
+from scipy.special import psi
+from nolitsa import dimension
+import statsmodels.api as sm
+from scipy.stats import norm
+import pmdarima as pm
 import pandas as pd
+import numpy as np
+import statistics
+import warnings
+import nolds
+import os
 
-filename = 'data/DublinAirport_Data.csv'
+# Suppress Warnings.
+warnings.filterwarnings("ignore")
 
+# Load Data.
+filename = 'Data/DublinAirport_Data.csv'
 try:
     data = pd.read_csv(filename, delimiter=',', parse_dates=['date'])
 except FileNotFoundError:
     os.system('python DublinAirportDataPreprocessing.py')
     data = pd.read_csv(filename, delimiter=',', parse_dates=['date'])
-print("---------------------------------------------------------------------------------------------------------------")
-print("Time Series:")
+print("===============================================================================================================")
+print("Average Temperature Time Series:\n")
+savepath = 'Figures/AirportLin'
 data = data.drop("Unnamed: 0", axis=1)
 print(data)
+dates = data.date
+date_axis = [d.to_pydatetime() for d in dates]
+
+# Define Average Temperature Time Series.
+x = data.AvgTemp.values
+x_df = pd.DataFrame({data.AvgTemp.name: x})
+m = len(x_df)
+
+# Average Temperature Autocorrelation of Original Time Series.
+plot_acf(x, zero=False)
+title = 'Autocorrelation of Original Time Series'
+plt.title(title, x=0.5, y=1.0)
+plt.xlabel('lag(tau)')
+plt.savefig(f'{savepath}/{title}.png')
+plt.show(block=False)
+
+# Stationary Time Series.
+window = 92
+ma = lf.rolling_window(x=x, window=window)
+plt.figure()
+plt.plot(ma, linestyle='--')
+plt.plot(x, alpha=0.5)
+plt.legend([f'MA ({window})', 'Original'])
+title = f'Moving Average ({window})'
+plt.xlabel('Time (Days)')
+plt.ylabel('AvgTemp (°C)')
+plt.title(title, x=0.5, y=1.0)
+plt.xlim(15000, 18000)
+plt.savefig(f'{savepath}/{title}.png')
+plt.show(block=False)
+fd = np.log(mas + abs(min(mas)) + 1)
+fd = fd - fd.mean()
+plt.figure()
+plt.plot(fd, linestyle='--')
+plt.legend(['Log(X_Detrended+abs(min(X_Detrended)) + 1)-mean'])
+title = 'Logarithms of Detrended Average Temperature Time Series'
+plt.xlabel('Time (Days)')
+plt.title(title, x=0.5, y=1.0)
+plt.savefig(f'{savepath}/{title}.png')
+plt.show(block=False)
+plt.figure()
+plt.plot(fd)
+plt.legend(['Log(X_Detrended+abs(min(X_Detrended)) + 1)-mean'])
+title = 'Logarithms of Detrended Time Series (Increased Resolution)'
+plt.xlabel('Time (Days)')
+plt.title(title, x=0.5, y=1.0)
+plt.xlim(15000, 22000)
+plt.savefig(f'{savepath}/{title}.png')
+plt.show(block=False)
+
+# Hypothesis test for white noise after Detrending with MA (92) and taking the logs.
+# Autocorrelation.
+fd = fd[15000:16000]
+maxtau = 31
+acvf = lf.get_acf(fd, lags=maxtau)
+title = 'Autocorrelation of log(X_detrended)'
+plt.title(title, x=0.5, y=1.0)
+plt.savefig(f'{savepath}/{title}.png')
+plt.show(block=False)
+
+# Model Adaption and Forecasting Single Step and Multistep.
+# AR Model.
+# Partial Autocorrelation Criterion for choosing model order.
+pacvf = lf.get_pacf(fd, lags=maxtau)
+title = 'Partial Autocorrelation of log(X_detrended)'
+plt.title(title, x=0.5, y=1.0)
+plt.savefig(f'{savepath}/{title}.png')
+plt.show(block=False)
+plt.pause(0.001)
+
+# Model AR(6).
+p_ar = 6
+summary_ar, fittedvalues_ar, resid_ar, model_ar, aic_ar = lf.fit_arima_model(x=fd, p=p_ar, q=0, savepath=savepath,
+                                                                             d=0, show=True)
+plt.pause(0.001)
+print("===============================================================================================================")
+print("Summary of chosen AR Model:\n")
+print(summary_ar)
+plt.pause(0.001)
+
+# Portmanteau Test to see if the residuals are white noise.
+lf.portmanteau_test(resid_ar, maxtau, best_p_ar, 0, 0, savepath, show=True)
+
+n = 100
+x0 = 0.51
+r = 3.9
+xV = nlf.logisticmap(n=n, r=r, x0=x0)
+plt.figure()
+plt.plot(xV)
+# embedded = nlf.embed_data(xV, order=2, delay=1)
+
+plt.scatter(xV[:-1], xV[1:])
+xV = nlf.generate_arma_ts([0.8], [0], n)
+plt.plot(xV)
+plt.show()
+embedded = nlf.embed_data(xV, order=2, delay=1)
+print(embedded)
+
+plt.scatter(xV[:-1], xV[1:])
+xM = data.henon()
+plt.plot(xM)
+plt.figure(figsize=(14, 8))
+xM = xM[:, 0]
+plt.plot(xM)
+embedded = nlf.embed_data(xM, order=3, delay=1)
+nlf.plot_3d_attractor(embedded)
+
+# REAL DATA ATTRACTORS
+os.chdir('d:/timeserieslab/')
+df = pd.read_csv('./data/BTCUSDT.csv')
+df.set_index(pd.to_datetime(df['time']), inplace=True)
+df.drop('time', axis=1, inplace=True)
+df = df['01-01-2020':'12-31-2021']
+df.plot()
+logreturns = df.apply(lambda x: np.log(x)).diff().bfill()
+logreturns.plot()
+embedded = nlf.embed_data(logreturns.values.reshape(-1, ), order=2, delay=1)
+plt.scatter(embedded[:-1], embedded[1:])
+eeg = pd.read_csv('./data/epileeg.dat')
+# eeg.plot();
+embedded = nlf.embed_data(eeg.values.reshape(-1, ), order=3, delay=1)
+nlf.plot_3d_attractor(embedded)
+# plt.scatter(embedded[:-1], embedded[1:], linestyle='--', marker='x');
+
+# Check for stationarity (Visual Inspection + statistical test)
+xM = xM[2900:3000]
+plt.figure(figsize=(14, 8))
+plt.plot(xM)
+
+adf = adfuller(xM, maxlag=None)
+print(adf)
+
+# Compute autocorrelation and delayed mutual information.
+print(dir(delay))
+help(delay.acorr)
+# xM.shape
+
+maxtau = 10
+lag = np.arange(maxtau)
+r = delay.acorr(xM, maxtau=maxtau)
+i = delay.dmi(xM, maxtau=maxtau)
+r_delay = np.argmax(r < 1.0 / np.e)
+
+plt.figure(1, figsize=(14, 8))
+
+plt.subplot(211)
+plt.title(r'Delay estimation for Henon map')
+plt.ylabel(r'Delayed mutual information')
+plt.plot(lag, i, marker='o')
+
+plt.subplot(212)
+plt.xlabel(r'Time delay $\tau$')
+plt.ylabel(r'Autocorrelation')
+# plt.plot(lag, r, r_delay, r[r_delay], 'o')
+plt.axhline(1.0 / np.e, linestyle='--', alpha=0.7, color='red')
+
+plt.figure(2, figsize=(14, 8))
+plt.subplot(111)
+plt.title(r'Time delay = %d' % r_delay)
+plt.xlabel(r'$x(t)$')
+plt.ylabel(r'$x(t + \tau)$')
+plt.plot(xM[:-r_delay], xM[r_delay:], '.')
+
+plt.show()
+print(r)
+print(r'Autocorrelation time = %d' % r_delay)
+
+dim = np.arange(1, 10 + 1)
+f1, f2, f3 = dimension.fnn(xM, tau=1, dim=dim, window=10, metric='cityblock')
+
+plt.title(r'FNN for Henon map')
+plt.xlabel(r'Embedding dimension $d$')
+plt.ylabel(r'FNN (%)')
+plt.plot(dim, 100 * f1, 'bo--', label=r'Test I')
+plt.plot(dim, 100 * f2, 'g^--', label=r'Test II')
+plt.plot(dim, 100 * f3, 'rs-', label=r'Test I + II')
+plt.legend()
+plt.show()
+
+# Correlation Sum
+plt.figure(figsize=(14, 8))
+plt.title('Local $D_2$ vs $r$ for Henon map')
+plt.xlabel(r'Distance $r$')
+plt.ylabel(r'Local $D_2$')
+theiler_window = 10
+tau = 1
+dim = np.arange(1, 10 + 1)
+
+for r, c in d2.c2_embed(xM, tau=tau, dim=dim, window=theiler_window,
+                        r=utils.gprange(0.001, 1.0, 100)):
+    plt.semilogx(r[3:-3], d2.d2(r, c), color='#4682B4')
+
+plt.semilogx(utils.gprange(0.001, 1.0, 100), 1.220 * np.ones(100),
+             color='#000000')
+plt.show()
+
+r = utils.gprange(0.001, 1.0, 100)
+corr_dim, debug_data = nolds.corr_dim(xM, emb_dim=2, rvals=r, debug_data=True)
+
+# Values used for log(r).
+rvals = debug_data[0]
+
+# The corresponding log(C(r)).
+csums = debug_data[1]
+
+# Line coefficients ([slope, intercept]).
+poly = debug_data[2]
+
+fig = plt.figure(figsize=(14, 8))
+ax = fig.add_subplot(111)
+ax.plot(rvals, csums)
+ax.set_xlabel('log(r)')
+ax.set_ylabel('log(C(r))')
+
+# Predictions
+# - LAP
+# - LLP
+n = xM.shape[0]
+test_prop = 0.3
+split_point = int(n * (1 - test_prop))
+
+train_xM = xM[:split_point]
+test_xM = xM[split_point:]
+
+plt.figure(figsize=(16, 8))
+plt.plot(np.arange(split_point), train_xM, color='blue', alpha=0.7, label='train set')
+plt.plot(np.arange(split_point, xM.shape[0]), test_xM, color='red', linestyle='--', alpha=0.7, label='test set')
+plt.legend()
+plt.xlabel('Time')
+plt.ylabel('Value')
+
+embed_train_data = nlf.embed_data(train_xM, 2, 1)
+embed_test_data = nlf.embed_data(test_xM, 2, 1)
+
+knn = 5
+tree = KDTree(embed_train_data, leaf_size=1, metric='chebyshev')
+neighbors_idx = []
+for i, state in enumerate(embed_test_data):
+    dist, neigh_idx = tree.query(state.reshape(1, -1), k=knn)
+    neighbors_idx.append(tuple([i, neigh_idx[0]]))
+
+plt.figure(figsize=(16, 8))
+plt.plot(xM)
+plt.xlabel('time')
+plt.ylabel('xM')
+plt.axvline(split_point, linestyle='--', color='black', alpha=0.5)
+# Get neighbors of first test datapoint.
+check_test_point = 1
+neigh = neighbors_idx[check_test_point]
+print(neigh)
+test_state_idx = neigh[0]
+neighs_idx = neigh[1]
+plt.plot([neighs_idx, neighs_idx + 1], [xM[neighs_idx], xM[neighs_idx + 1]], linestyle='--', color='orange')
+plt.scatter([neighs_idx, neighs_idx + 1], [xM[neighs_idx], xM[neighs_idx + 1]], linestyle='--', color='orange')
+# Test Set State.
+plt.plot([test_state_idx + split_point, test_state_idx + split_point + 1],
+         [xM[test_state_idx + split_point], xM[test_state_idx + split_point + 1]], linestyle='--', color='red')
+plt.scatter([test_state_idx + split_point, test_state_idx + split_point + 1],
+            [xM[test_state_idx + split_point], xM[test_state_idx + split_point + 1]], linestyle='--', color='red')
+
+plt.figure(figsize=(16, 8))
+plt.scatter(embed_train_data[:, 0], embed_train_data[:, 1], alpha=0.3)
+plt.scatter(embed_train_data[neighs_idx][:, 0], embed_train_data[neighs_idx][:, 1], label=f'neighbors in train data')
+plt.scatter(embed_test_data[test_state_idx, 0], embed_test_data[test_state_idx, 1],
+            label=f'test point:{test_state_idx}')
+plt.xlabel('x(t)')
+plt.ylabel('x(t+1)')
+plt.legend()
+
+T = 1
+lap_predictions = []
+for neigh in neighbors_idx:
+    test_state_idx = neigh[0]
+    neighs_idx = neigh[1]
+    images_idx = neighs_idx + T
+    images = xM[images_idx]
+    lap = np.sum(images) / len(images)
+    lap_predictions.append(lap)
+plt.figure(figsize=(14, 8))
+plt.plot(np.arange(split_point + 1, xM.shape[0]), lap_predictions, label='LAP prediction', alpha=0.9, linestyle='-.')
+plt.plot(np.arange(split_point + 1, xM.shape[0]), test_xM[1:], label='True values', alpha=0.9, linestyle='--')
+
+np.sqrt(np.mean((np.array(lap_predictions) - test_xM[1:]) ** 2)) / np.std(test_xM[1:])
+
+X, y = [], []
+for neigh in neighs_idx:
+    x_ = [xM[neigh], xM[neigh + 1]]
+    y_ = [xM[neigh + 1 + T]]
+    X.append(x_)
+    y.append(y_)
+
+X = sm.add_constant(X)
+X = np.asarray(X)
+y = np.asarray(y)
+ols = sm.OLS(endog=y, exog=X).fit()
+llp = np.dot(ols.params, [1, xM[i + split_point], xM[i + split_point + 1]])
+ols.summary()
+
+# Real Data
+with open('epileeg.dat', 'r') as file:
+    lines = file.readlines()
+xM = np.full(shape=len(lines), fill_value=np.nan)
+for i, line in enumerate(lines):
+    point = line.rstrip().lstrip()
+    xM[i] = point
+xM = np.array(xM)
+plt.plot(xM)
+plt.show()
+
---
+++ NonLinearAnalysisFunctions.py
@@ -0,0 +1,370 @@
+# Imports.
+from statsmodels.tsa.arima_process import arma_generate_sample, arma_acf, arma_pacf
+from statsmodels.tsa.stattools import acf, pacf, adfuller
+from statsmodels.graphics.tsaplots import plot_acf
+from nolitsa import delay, dimension, d2, utils
+from sklearn.metrics import mean_squared_error
+import NonLinearAnalysisFunctions as nlf
+import LinearAnalysisFunctions as lf
+from matplotlib import pyplot as plt
+from sklearn.neighbors import KDTree
+from statsmodels.api import OLS
+from scipy.special import psi
+from nolitsa import dimension
+import statsmodels.api as sm
+from scipy.stats import norm
+import pmdarima as pm
+import pandas as pd
+import numpy as np
+import statistics
+import warnings
+import nolds
+import os
+
+
+# Theoretical autocorrelation function of an ARMA process. phiV: The coefficients for autoregressive lag
+# polynomial, not including zero lag. thetaV : array_like, 1d. The coefficients for moving-average lag polynomial, not
+# including zero lag.
+def armacoefs2autocorr(phiv, thetav, lags=10):
+    phiv, thetav = np.array(phiv), np.array(thetav)
+    phiv = np.r_[1, -phiv]  # add zero lag
+    thetav = np.r_[1, thetav]  # #add zero lag
+    acf_ = arma_acf(phiv, thetav, lags=lags)
+    fig, ax = plt.subplots(1, 1, figsize=(14, 8))
+    ax.scatter(np.arange(1, lags), acf_[1:], marker='o')
+    ax.set_xlabel('Lags')
+    ax.set_xticks(np.arange(1, lags))
+    ax.set_yticks(np.arange(-1, 1, 0.1))
+    ax.set_title('ACF', fontsize=14)
+    ax.grid(linestyle='--', linewidth=0.5, alpha=0.15)
+    plt.show()
+    # for t in np.arange(lags):
+    #     ax.axvline(t, ymax=acf_[t], color='red', alpha=0.3)
+
+
+def macoef2autocorr(phiv, thetav, lags=10):
+    pacf_ = arma_pacf(phiv, thetav, lags=10)
+    fig, ax = plt.subplots(1, 1)
+    ax.scatter(np.arange(lags), pacf_, marker='o')
+    for t in np.arange(lags):
+        ax.axvline(t, ymax=pacf_[t], color='red', alpha=0.3)
+
+
+# Plot 3d attractor.
+def plot_3d_attractor(xm):
+    fig = plt.figure(figsize=(14, 8))
+    ax = fig.add_subplot(111, projection='3d')
+    ax.scatter(xm[:, [0]], xm[:, [1]], xm[:, [2]])
+    plt.show()
+
+
+# Time-delay embedding. x : 1d-array, shape (n_times) Time series. m : int Embedding dimension (order).
+# tau : int Delay. Returns embedded : ndarray, shape (n_times - (order - 1) * delay, order) Embedded time-series.
+def embed_data(xv, m=3, tau=1):
+    n = len(xv)
+    nvec = n - (m - 1) * tau
+    xm = np.zeros(shape=(nvec, m))
+    for i in np.arange(m):
+        xm[:, m - i - 1] = xv[i * tau:nvec + i * tau]
+    return xm
+
+
+# Start prediction after getting all lags needed from model.
+def predict_multistep(model, tmax=10, show=False):
+    tmin = np.max([len(model.arparams), len(model.maparams), 1])
+    preds = model.predict(start=tmin, end=tmax, dynamic=True)
+    if show:
+        fig, ax = plt.subplots(1, 1, figsize=(14, 8))
+        ax.plot(preds)
+        ax.set_title('Multistep prediction')
+        ax.set_xlabel('T')
+        plt.show()
+    return preds
+
+
+# Transform a variable of any distribution into normal.
+def gaussianisation(data):
+    sort_ind = np.argsort(data)
+    gaussian_data = np.random.normal(0, 1, size=data.shape[0])
+    gaussian_data_ind = np.argsort(gaussian_data)
+    g_d_sorted = gaussian_data[gaussian_data_ind]
+    y = np.zeros(shape=data.shape[0])
+    for i in np.arange(data.shape[0]):
+        y[i] = g_d_sorted[sort_ind[i]]
+    return y
+
+
+# Get NRMSE.
+def get_nrmse(target, predicted):
+    se = (target - predicted) ** 2
+    mse = np.mean(se)
+    rmse = np.sqrt(mse)
+    return rmse / np.std(target)
+
+
+# NRMSE Definition.
+def nrmse(truev, predictedv):
+    n = mean_squared_error(truev, predictedv, squared=False)/(max(truev)-min(truev))
+    return n
+
+
+# LINEARFITNRMSE fits an AR model and computes the fitting error for t-step ahead. INPUTS: xV: vector of the scalar
+# time series. m: the embedding dimension. Tmax    : the prediction horizon, the fit is made for t=1...Tmax steps ahead.
+# tittxt: string to be displayed in the title of the figure if not specified, no plot is made. OUTPUT: nrmsev: vector
+# of length Tmax, the nrmse of the fit for t-mappings, t=1...Tmax. phiv: the coefficients of the estimated AR time
+# series of length (m+1) with phi(0) as first component.
+def linearfitnrmse(xv, m, tmax=1, show=False):
+    n = xv.shape[0]
+    mx = np.mean(xv[:n - tmax + 1])
+    yv = xv[:n - tmax + 1] - mx
+    nvec = n - m - 1 - tmax
+    ym = np.full(shape=(nvec-1, m), fill_value=np.nan)
+    for j in np.arange(m):
+        ym[:, [m-j-1]] = yv[j:nvec+j-1]
+    rv = yv[m:nvec+m-1]
+    # np.linalg.lstsq(ym, rv)
+    ols = OLS(endog=rv, exog=ym).fit()
+    av = ols.params
+    a0 = (1 - np.sum(av)) * mx
+    phiv = np.r_[a0, av]
+    prem = np.full(shape=(n + tmax - 1, tmax), fill_value=np.nan)
+    for i in np.arange(m, n):
+        prev = np.full(shape=(m + tmax, 1), fill_value=np.nan)
+        prev[:m] = xv[i - m: i] - mx
+        for T in np.arange(1, tmax + 1):
+            prev[m + T - 1] = np.dot(av, (prev[T-1:m+T-1][::-1]))
+            prem[i + T - 1, [T-1]] = prev[m + T - 1]
+    prem = prem + mx
+    nrmsev = np.ones(shape=(tmax, 1))
+    for T in np.arange(1, tmax + 1):
+        nrmsev[T-1] = nrmse(xv[m + T - 1:n], prem[m + T - 1: n, [T - 1]])
+    if show:
+        fig, ax = plt.subplots(1, 1)
+        ax.plot(np.arange(1, tmax + 1), nrmsev, marker='x')
+        ax.set_xlabel('prediction time T')
+        ax.set_ylabel('NRMSE(T)')
+    return nrmsev, phiv
+
+
+# LOCALFITNRMSE makes fitting using a local model of zeroth order (average mapping or nearest neighbor mappings if only
+# one neighbor is chosen) or a local linear model and computes the fitting error for t-step ahead. For the search for
+# neighboring points it uses the Matlab k-d-tree search. The fitting here means that predictions are made for all the
+# points in the data set (in-sample prediction). The prediction error statistic (NRMSE measure) for the t-step ahead
+# predictions is the goodness-of-fit statistic. The state space reconstruction is done with the method of delays having
+# as parameters the embedding dimension 'm' and the delay time 'tau'. The local prediction model is one of the
+# following: Ordinary Least Squares, OLS (standard local linear model): if the truncation parameter q >= m. Principal
+# Component Regression, PCR, project the parameter space of the model to only q of the m principal axes: if 0<q<m. Local
+# Average Mapping, LAM: if q=0. The local region is determined by the number of neighbours 'nnei'. The k-d-tree data
+# structure is utilized to speed up computation time in the search of neighboring points and the implementation of
+# Matlab is used. INPUTS: xV: vector of the scalar time series. tau: the delay time (usually set to 1). m: the embedding
+# dimension. Tmax: the prediction horizon, the fit is made for t=1...Tmax steps ahead. nnei: number of nearest neighbors
+# to be used in the local model. If k=1,the nearest neighbor mapping is the fitted value. If k>1, the model as defined
+# by the input patameter 'q' is used. q: the truncation parameter for a normalization of the local linear model if
+# specified (to project the parameter space of the model, using Principal Component Regression, PCR, locally). if
+# q>=m -> Ordinary Least Squares, OLS (standard local linear model, no projection). if 0<q<m -> PCR(q). if q=0 ->
+# local average model (if in addition nnei=1 -> then the zeroth order model is applied). tittxt  : string to be
+# displayed in the title of the figure if not specified, no plot is made. OUTPUT: nrmsev: vector of length Tmax, the
+# nrmse of the fit for t-mappings, t=1...Tmax. prem: the matrix of size nvec x (1+Tmax) having the fit (in-sample
+# predictions) for t=1,...,Tmax, for each of the nvec reconstructed points from the whole time series. The first column
+# has the time of the target point and the rest Tmax columns the fits for t=1,...,Tmax time steps ahead.
+def localfitnrmse(xv, tau, m, tmax, nnei, q, show=''):
+    if q > m:
+        q = int(m)
+    n = xv.shape[0]
+    if n < 2 * (m-1)*tau - tmax:
+        print('too short timeseries')
+        return
+    nvec = n - (m-1) * tau - tmax
+    xm = np.full(shape=(nvec, m), fill_value=np.nan)
+    for j in np.arange(m):
+        xm[:, [m-j-1]] = xv[j * tau:nvec + j * tau]
+    from scipy.spatial import KDTree
+    kdtrees = KDTree(xm)
+    prem = np.full(shape=(nvec, tmax), fill_value=np.nan)
+    _, nneiindm = kdtrees.query(xm, k=nnei+1, p=2)
+    nneiindm = nneiindm[:, 1:]
+    for i in np.arange(nvec):
+        neim = xm[nneiindm[i]]
+        yv = xv[nneiindm[i] + m * tau]
+        if q == 0 or nnei == 1:
+            prem[i, 0] = np.mean(yv)
+        else:
+            mneiv = np.mean(neim, axis=0)
+            my = np.mean(yv)
+            zm = neim - mneiv
+            [ux, sx, vx] = np.linalg.svd(zm, full_matrices=False)
+            sx = np.diag(sx)
+            vx = vx.T
+            tmpm = vx[:, :q] @ (np.linalg.inv(sx[:q, :q]) @ ux[:, :q].T)
+            lsbv = tmpm @ (yv - my)
+            prem[i] = my + (xm[i, ] - mneiv) @ lsbv
+    if tmax > 1:
+        winnowm = np.full(shape=(nvec, (m - 1) * tau + 1), fill_value=np.nan)
+        for i in np.arange(m*tau):
+            winnowm[:, [i]] = xv[i:nvec + i]
+        for t in np.arange(2, tmax + 1):
+            winnowm = np.concatenate([winnowm, prem[:, [t-2]]], axis=1)
+            targm = winnowm[:, :-(m+1)*tau:-tau]
+            _, nneiindm = kdtrees.query(targm, k=nnei, p=2)
+
+            for i in np.arange(nvec):
+                neim = xm[nneiindm[i], :]
+                yv = xv[nneiindm[i] + (m - 1) * tau + 1]
+                if q == 0 or nnei == 1:
+                    prem[i, t-1] = np.mean(yv)
+                else:
+                    mneiv = np.mean(neim, axis=0)
+                    my = np.mean(yv)
+                    zm = neim - mneiv
+                    [ux, sx, vx] = np.linalg.svd(zm, full_matrices=False)
+                    sx = np.diag(sx)
+                    vx = vx.T
+                    tmpm = vx[:, :q] @ (np.linalg.inv(sx[:q, :q]) @ ux[:, :q].T)
+                    lsbv = tmpm @ (yv - my)
+                    prem[i, t-1] = my + (targm[i, :] - mneiv) @ lsbv
+    nrmsev = np.full(shape=(tmax, 1), fill_value=np.nan)
+    idx = (np.arange(nvec) + (m-1)*tau).astype(np.int)
+    for t_idx in np.arange(1, tmax + 1):
+        nrmsev[t_idx-1] = nrmse(truev=xv[idx + t_idx, ], predictedv=prem[:, [t_idx - 1]])
+    if show:
+        fig, ax = plt.subplots(1, 1)
+        ax.plot(np.arange(1, tmax + 1), nrmsev, marker='x')
+        ax.set_xlabel('prediction time t')
+        ax.set_ylabel('NRMSE(t)')
+    return nrmsev, prem
+
+
+# Plot Time Series.
+def plot_timeseries(xv, get_histogram=False, title='', savepath=''):
+    fig, ax = plt.subplots(1, 1, figsize=(14, 8))
+    ax.plot(xv, marker='x', linestyle='--', linewidth=2)
+    ax.set_xlabel('time')
+    ax.set_ylabel('value')
+    if len(title) > 0:
+        ax.set_title(title, x=0.5, y=1.0)
+    plt.tight_layout()
+    if len(savepath) > 0:
+        plt.savefig(f'{savepath}/{title}_xM.jpeg')
+    # Plot Histogram.
+    if get_histogram:
+        fig, ax = plt.subplots(1, 1, figsize=(14, 8))
+        ax.hist(xv, alpha=0.8, rwidth=0.9)
+        ax.set_xlabel('value')
+        ax.set_title('Histogram')
+        plt.tight_layout()
+        if len(title) > 0:
+            ax.set_title(title, x=0.5, y=1.0)
+        plt.tight_layout()
+        if len(savepath) > 0:
+            plt.savefig(f'{savepath}/{title}_hist.jpeg')
+
+
+# Helper Function.
+def ann(x, k):
+    tree = KDTree(x, leaf_size=1, metric='chebyshev')
+    dists, nnidx = tree.query(x, k=k)
+    del tree
+    return nnidx, dists
+
+
+# Helper Function.
+def annr(x, rv):
+    tree = KDTree(x, leaf_size=1, metric='chebyshev')
+    nnnidx = tree.query_radius(x, r=rv, count_only=True)
+    return nnnidx
+
+
+# Helper Function.
+def nneighforgivenr(x, rv):
+    npv = annr(x, rv)
+    npv[npv == 0] = 1
+    return npv
+
+
+# Calculates I(X;Y) using KSG algorithm1 (with max-norms squares).
+def mi_estimator_ksg1(xv, yv, nnei=5, normalize=False):
+    n = xv.shape[0]
+    psi_nnei = psi(nnei)
+    psi_n = psi(n)
+    if normalize:
+        xv = (xv - np.min(xv)) / np.ptp(xv)
+        yv = (yv - np.min(yv)) / np.ptp(yv)
+    xembm = np.concatenate((xv, yv), axis=1)
+    _, distsm = ann(xembm, nnei + 1)
+    maxdistv = distsm[:, -1]
+    n_x = nneighforgivenr(x=xv, rv=maxdistv - np.ones(n) * 10 ** (-10))
+    n_y = nneighforgivenr(x=yv, rv=maxdistv - np.ones(n) * 10 ** (-10))
+    psibothm = psi(np.concatenate((n_x.reshape(-1, 1), n_y.reshape(-1, 1)), axis=1))
+    # I(X;Y) = ψ(k) + ψ(Ν) - <ψ(Nx + 1) + ψ(Ny + 1)>
+    mi = psi_nnei + psi_n - np.mean(np.sum(psibothm, axis=1))
+    return mi
+
+
+def falsenearestneighbors(xv, m_max=10, tau=1, show=False):
+    dim = np.arange(1, m_max + 1)
+    f1, _, _ = dimension.fnn(xv, tau=tau, dim=dim, window=10, metric='cityblock')
+    if show:
+        fig, ax = plt.subplots(1, 1, figsize=(14, 8))
+        ax.scatter(dim, f1)
+        ax.axhline(0.01, linestyle='--', color='red', label='1% threshold')
+        ax.set_xlabel(f'm')
+        ax.set_title(f'FNN ({m_max})')
+        ax.set_xticks(dim)
+        ax.legend()
+        plt.show()
+    return f1
+
+
+def correlationdimension(xv, mmax, show=False):
+    m_all = np.arange(1, mmax + 1)
+    corrdimv = []
+    logrm = []
+    logcrm = []
+    polym = []
+    for m in m_all:
+        corrdim, *corrData = nolds.corr_dim(xv, m, debug_data=True)
+        corrdimv.append(corrdim)
+        logrm.append(corrData[0][0])
+        logcrm.append(corrData[0][1])
+        polym.append(corrData[0][2])
+    if show:
+        fig, ax = plt.subplots(1, 1, figsize=(14, 8))
+        ax.plot(m_all, corrdimv, marker='x', linestyle='.-')
+        ax.set_xlabel('m')
+        ax.set_xticks(m_all)
+        ax.set_ylabel('v')
+        ax.set_title('Corr Dim vs m')
+    return corrdimv, logrm, logcrm, polym
+
+
+def split2train_testset(xv, test_proportion):
+    n = np.int(len(xv) * test_proportion)
+    return xv[:n], xv[n:]
+
+
+def logisticmap(n=1024, r=3., x0=None):
+    ntrans = 10
+    xv = np.full(shape=(n+ntrans, 1), fill_value=np.nan)
+    if x0 is None:
+        xv[0] = np.random.uniform(low=0, high=2.9)
+    else:
+        xv[0] = x0
+    for t in np.arange(1, n+ntrans):
+        xv[t] = r * xv[t-1] * (1 - xv[t-1])
+    xv = xv[ntrans:, [0]]
+    return xv.reshape(-1, )
+
+
+# Create an ARMA(p,q) time series of length 'n' with Gaussian input noise. Note that phiV = [phi(0) phi(1) ... phi(p)]'
+# and phi(0) is the constant term, and thetaV = [theta(1) ... theta(q)]'. sdnoise is the SD of the input noise (if left
+# out then sdnoise=1). The generating ARMA(p,q) process reads x(t) = phi(0) + phi(1)*x(t-1) + ... + phi(p)*x(t-p) +
+# + z(t) - theta(1)*z(t-1) + ... - theta(q)*z(t-p), z(t) ~ WN(0,sdnoise^2).
+def generate_arma_ts(phiv, thetav, n, sdnoise=1):
+    phiv = np.array(phiv)
+    thetav = np.array(thetav)
+    # Add Zero Lag.
+    ar_params = np.r_[1, -phiv[:]]
+    # Add Zero Lag.
+    ma_params = np.r_[1, thetav[:]]
+    xv = arma_generate_sample(ar=ar_params, ma=ma_params, nsample=n, scale=sdnoise, burnin=100)
+    return xv
